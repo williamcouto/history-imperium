047d047c46968873ff65bf9c7f54d28993240624
williamcouto <willplacido40@gmail.com>
2024-08-11 22:56:13 -0300

    Create JS
    fix modal
    style modals and images
    create script

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let modalsBttn = document.querySelectorAll('.modalBtn')
+
+// Adiciona eventos de clique para cada modal
+modalsBttn.forEach(function(button){
+    let idModal = button.getAttribute('data-modal')
+    let modal = document.getElementById(idModal)
+
+    if(idModal && modal){
+        button.addEventListener('click', function(){
+            modal.style.display = 'block'
+            modal.focus()
+        })
+    }
+})
